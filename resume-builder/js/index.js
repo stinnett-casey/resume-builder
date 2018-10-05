3b29f01053c094ac0703f22bc30328f360a7d4f4
Casey Stinnett <stinnett.casey@gmail.com>
2018-10-05 00:17:22 -0600

    moved files to root dir and added Chargeback exp

--- resume-builder/js/index.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$('li, #summary').click(function() {
-	  let text = prompt('Change value to what?', $(this).text().trim());
-	  if (text !== "" && text !== undefined && text !== null ) $(this).text(text);
-	});
-});
